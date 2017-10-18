1d911274e91cffd917bbd3d8a94cf95998ec86b3
Kirill <kirill.sidorchuk@teamdev.com>
2017-10-18 14:40:28 +0300

    Add finetuning ResNet50 stub

---
+++ Finetune.py
@@ -0,0 +1,18 @@
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential, Model
+from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
+from keras import backend as k
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
+
+
+img_width, img_height = 256, 256
+train_data_dir = "data/train"
+validation_data_dir = "data/val"
+nb_train_samples = 4125
+nb_validation_samples = 466
+batch_size = 16
+epochs = 50
+
+model = applications.ResNet50(weights = "imagenet", include_top=False, input_shape = (img_width, img_height, 3))
