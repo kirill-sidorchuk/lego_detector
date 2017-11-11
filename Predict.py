b0c80b8e8cbb21ab0bb69c440f435fc330f74c61
Kirill <kirill.sidorchuk@teamdev.com>
2017-11-11 16:48:11 +0200

    Better Predict script functionality: 2 different augmentations, 'sort' and 'measure' modes added

--- Predict.py
+++ Predict.py
@@ -1,11 +1,14 @@
 import argparse
 import csv
+from shutil import copyfile
+
 import numpy as np
 import os
 
 import cv2
 from keras.engine import Model
 
+from FilesAndDirs import clear_directory
 from Finetune import IMAGE_HEIGHT, IMAGE_WIDTH
 from ModelUtils import create_model_class
 
@@ -54,8 +57,147 @@ def create_int_to_labels_map(name_to_int):
     return int_to_name
 
 
+def predict_with_tta(tta, robot_tta, images, model):
+    tta_hflip = tta > 0
+    tta_vflip = tta > 1
+
+    results = []
+
+    if robot_tta:
+        tta_batch = []
+        for image_file in images:
+            # print("%s" % image_file)
+            data = load_image_data(image_file)
+            tta_batch.append(data)
+            if tta_hflip:
+                tta_batch.append(cv2.flip(data, 0))
+            if tta_vflip:
+                tta_batch.append(cv2.flip(data, 1))
+        probs_all = model.predict(np.array(tta_batch, dtype=np.float32))
+        results.append(np.mean(probs_all, axis=0))
+    else:
+        for image_file in images:
+            # print("%s" % image_file)
+            tta_batch = []
+            data = load_image_data(image_file)
+            tta_batch.append(data)
+            if tta_hflip:
+                tta_batch.append(cv2.flip(data, 0))
+            if tta_vflip:
+                tta_batch.append(cv2.flip(data, 1))
+            probs_all = model.predict(np.array(tta_batch, dtype=np.float32))
+
+            # averaging probabilities
+            probs = np.mean(probs_all, axis=0)
+            results.append(probs)
+
+    return results
+
+
+def sort_images(test_dir, tta, model):
+    """ Sort images with unknown labels to directories by predicted label"""
+
+    # preparing sorted dir
+    sorted_dir = os.path.join(test_dir, "sorted")
+    if not os.path.exists(sorted_dir):
+        os.mkdir(sorted_dir)
+    else:
+        clear_directory(sorted_dir)
+
+    files = os.listdir(test_dir)
+    image_files = []
+    for file in files:
+        path = os.path.join(test_dir, file)
+        if not os.path.isdir(path):
+            image_files.append(path)
+
+    all_probs = predict_with_tta(tta, False, image_files, model)
+
+    for i in range(len(image_files)):
+        probs = all_probs[i]
+        src_image = image_files[i]
+
+        src_image_name = os.path.split(src_image)[1]
+        predicted_label_index = np.argmax(probs)
+        prob = probs[predicted_label_index]
+        if prob < 0.1:
+            predicted_label = "unknown"
+        else:
+            predicted_label = int_to_labels_map[predicted_label_index]
+
+        print("%s: %s" % (src_image_name, predicted_label))
+        label_dir = os.path.join(sorted_dir, predicted_label)
+        if not os.path.exists(label_dir):
+            os.mkdir(label_dir)
+        dst_image_path = os.path.join(label_dir, src_image_name)
+        copyfile(src_image, dst_image_path)
+
+
+def measure_accuracy(test_dir, tta, robot_tta, model, labels_map, int_to_labels_map):
+
+    dirs = os.listdir(test_dir)
+    test_dirs = []
+    for d in dirs:
+        path = os.path.join(test_dir, d)
+        if not os.path.isdir(path) or d not in labels_map:
+            # skipping files and unknown labels
+            continue
+        test_dirs.append(d)
+
+    print("%d label directories found" % len(test_dirs))
+
+    top_n = 5
+    top_1_acc = 0
+    top_5_acc = 0
+    N = 0
+
+    for label_dir in test_dirs:
+
+        print("\n%s/" % label_dir)
+
+        # reading label dir
+        label_path = os.path.join(test_dir, label_dir)
+        files = os.listdir(label_path)
+        image_files = []
+        for f in files:
+            path = os.path.join(label_path, f)
+            if os.path.isfile(path):
+                image_files.append(path)
+
+        if robot_tta > 1:
+            n_batches = len(image_files) // robot_tta
+            for i in range(n_batches):
+                offset = i * robot_tta
+                robot_images = image_files[offset: offset + robot_tta]
+                probs = predict_with_tta(tta, True, robot_images, model)[0]
+
+                for im in robot_images:
+                    print("\t%s" % os.path.split(im)[1])
+
+                sorted_indexes = np.argsort(probs)
+
+                true_label = label_dir
+                top_5_hit = False
+                for t in range(top_n):
+                    label_index = sorted_indexes[-t-1]
+                    label_prob = probs[label_index]
+                    label_name = int_to_labels_map[label_index]
+                    if t == 0 and label_name == true_label:
+                        top_1_acc += 1
+                    if t < 5 and label_name == true_label:
+                        top_5_hit = True
+                    print("%1.2f%% %s" % (label_prob*100.0, label_name))
+
+                if top_5_hit:
+                    top_5_acc += 1
+                N += 1
+
+    print("top 1 accuracy = %1.2f%%" % (top_1_acc*100.0/N))
+    print("top 5 accuracy = %1.2f%%" % (top_5_acc*100.0/N))
+
+
 def test(args):
-    test_dir = os.path.join(args.data_root, TEST_DIR)
+    test_dir = os.path.join(args.data_root, args.image_dir)
 
     snapshot_path = os.path.join(args.data_root, SNAPSHOTS_PATH, args.model)
     labels_map = load_labels_to_int_mapping(os.path.join(snapshot_path, "labels.csv"))
@@ -64,60 +206,26 @@ def test(args):
     num_classes = len(labels_map)
     print("%d classes for model %s" % (num_classes, args.model))
 
-    # reading test directory
-    test_images = os.listdir(test_dir)
-    print("%d test images found" % len(test_images))
-    test_images_labels = parse_images_labels(test_images)
-
     # loading model
     model_obj = create_model_class(args.model)
     model = model_obj.create_model(IMAGE_WIDTH, IMAGE_HEIGHT, num_classes)
     print("loading weights...")
     model.load_weights(os.path.join(snapshot_path, args.snapshot), by_name=True)
 
-    tta_hflip = True
-    tta_vflip = True
+    if args.mode.lower() == "sort":
+        sort_images(test_dir, args.tta, model)
+    elif args.mode.lower() == "measure":
+        measure_accuracy(test_dir, args.tta, args.rtta, model, labels_map, int_to_labels_map)
 
-    top_n = 5
-    top_1_acc = 0
-    top_5_acc = 0
-    for filename in test_images:
-        tta_batch = []
-        data = load_image_data(os.path.join(test_dir, filename))
-        tta_batch.append(data)
-        if tta_hflip:
-            tta_batch.append(cv2.flip(data, 0))
-        if tta_vflip:
-            tta_batch.append(cv2.flip(data, 1))
-        probs_all = model.predict(np.array(tta_batch, dtype=np.float32))
-
-        # averaging probabilities
-        probs = np.mean(probs_all, axis=0)
-
-        sorted_indexes = np.argsort(probs)
-        print("\n%s:" % filename)
-        true_label = parse_filename_label(os.path.splitext(filename)[0])
-        top_5_hit = False
-        for t in range(top_n):
-            label_index = sorted_indexes[-t-1]
-            label_prob = probs[label_index]
-            label_name = int_to_labels_map[label_index]
-            if t == 0 and label_name == true_label:
-                top_1_acc += 1
-            if t < 5 and label_name == true_label:
-                top_5_hit = True
-            print("%1.2f%% %s" % (label_prob*100.0, label_name))
-
-        if top_5_hit:
-            top_5_acc += 1
-
-    print("top 1 accuracy = %1.2f%%" % (top_1_acc*100.0/len(test_images)))
-    print("top 5 accuracy = %1.2f%%" % (top_5_acc*100.0/len(test_images)))
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Finetune model')
     parser.add_argument("data_root", type=str, help="data root dir")
+    parser.add_argument("image_dir", type=str, help="directory with images for prediction (relative path to data_root)")
+    parser.add_argument("mode", type=str, default="measure", help="predict mode: measure or sort")
+    parser.add_argument("--tta", type=int, default=0, help="0 - no TTA, 1 - hflip, 2 - vflip+hflip")
+    parser.add_argument("--rtta", type=int, default=0, help="Robot TTA. <1 - no TTA, >1 - number of images to take for TTA")
     parser.add_argument("--model", type=str, help="model")
     parser.add_argument("--snapshot", type=str, help="snapshot")
 
