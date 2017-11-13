dc33402b522d972846efbd62683c3bd910928baa
Kirill <kirill.sidorchuk@teamdev.com>
2017-11-13 23:47:55 +0200

    Add model F: InceptionV3

---
+++ model_F.py
@@ -0,0 +1,24 @@
+from keras import applications
+from keras.engine import Model
+from keras.layers import Flatten, Dense, BatchNormalization, Activation, Dropout
+
+
+class Model_F(object):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def create_model(self, image_width, image_height, num_classes):
+        model = applications.InceptionV3(weights="imagenet", include_top=False, input_shape=(image_width, image_height, 3))
+
+        x = model.output
+        x = Flatten()(x)
+
+        x = Dropout(0.7)(x)
+
+        x = Dense(num_classes)(x)
+        x = BatchNormalization()(x)
+        x = Activation('softmax')(x)
+
+        predictions = x
+        return Model(model.input, predictions)
